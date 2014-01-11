4b592e86f1051b67ae00b188d6f3a37344ed1b99
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2014-01-10 18:33:52 -0800

    make some stuff a bit more accurate, make it build.

--- stack.rs
+++ stack.rs
@@ -1,29 +1,10 @@
-trait ImmStack<T> {
-    fn push(self, item : T) -> Self;
-    fn pop(self) -> (Option<T>, Option<Self>);
-    fn new() -> Self;
-}
-
 #[deriving(Eq, ToStr)]
 enum Chain<T> {
     Link(T, ~Chain<T>),
     Break
 }
 
-impl<T> ImmStack<T> for Chain<T> {
-    fn push(self, item : T) -> Chain<T> {
-        Link(item, ~self)
-    }
-    fn pop(self) -> (Option<T>, Option<Chain<T>>) {
-        match self {
-            Link(item, ~new_self) => return (Some(item), Some(new_self)),
-            Break => return (None, None)
-        }
-    }
-    fn new() -> Chain<T> {
-        Break
-    }
-}
+// this is lies memory usage wise, it's really a linked list. I should rename it.
 
 trait MutStack<T> {
     fn push(self, item : T) -> ~Self;
